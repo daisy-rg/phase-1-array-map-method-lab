73e7396788eadb399037a1cd7863f423731af63b
daisy <chpdaisy@gmail.com>
2025-03-19 12:21:32 +0300

    donr

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tutorials = [
+const tutorials =new Map [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = ()=> {
+
+  return tutorials.map(tutorial=>{
+    return tutorial
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() ;
+});
+  
 }
